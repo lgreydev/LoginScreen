1d04d3783c0143df5e01c3dc355f8c3a99129efc
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2021-09-02 12:23:46 +0300

    Add sign in with apple

--- UnderstandingAutoLayout/ViewController.swift
+++ UnderstandingAutoLayout/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet var viewOfLogo: UIView!
+    @IBOutlet var viewOfPassword: UIView!
+    @IBOutlet var viewSignApple: UIView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Setup View
+        viewOfLogo.layer.cornerRadius = 20
+        viewOfPassword.layer.cornerRadius = 20
+        viewSignApple.layer.cornerRadius = 20
     }
 
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+            .lightContent
+        }
 
 }
 
